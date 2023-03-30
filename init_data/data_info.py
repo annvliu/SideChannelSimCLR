fc74094431bf9d088197b78f4bdc35abdf385f7f
liuannv <18811653621@163.com>
2023-03-30 23:49:50 +0800

    20230330 增添数据集EM_trs_500，增添数据集信息

--- init_data/data_info.py
+++ init_data/data_info.py
@@ -21,4 +21,15 @@ def get_data_info(dataset_name):
                           'plain_fname': 'EM_cipher.npy',
                           'ascad_cnn_block_anti_bn_dense_input': 1024,
                           'ascad_cnn_dense_input': 1024}
+    dataset_para['EM_500'] = {  # filter = 1, shift = 5, cut = 5, 目前最好的是只用filter shift
+                            'dataset_name': 'EM',
+                            'feature_num': 90,
+                            'leakage_model': 'HD',
+                            'classification': 9,
+                            'true_key': 19,
+                            'trs_fname': 'EM_trs_500.npy',
+                            'label_fname': 'EM_label.npy',
+                            'plain_fname': 'EM_cipher.npy',
+                            'ascad_cnn_block_anti_bn_dense_input': 1024, ############
+                            'ascad_cnn_dense_input': 1024}
     return dataset_para[dataset_name]
