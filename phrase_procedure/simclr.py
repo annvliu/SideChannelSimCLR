8f0898d275aca9f861f49ca4a2afd88474a01cd5
liuannv <18811653621@163.com>
2024-01-26 16:52:39 +0800

    (fix) simclr: 简化info_nce_loss计算过程,提效

--- phrase_procedure/simclr.py
+++ phrase_procedure/simclr.py
@@ -23,34 +23,58 @@ class SimCLR(object):
         self.scheduler = kwargs['scheduler']
         self.criterion = torch.nn.CrossEntropyLoss().to(self.config['common']['device'])
 
+    # def info_nce_loss(self, features):
+    #     labels = torch.cat([torch.arange(self.config['common']['batch_size']) for i in range(self.config['n_views'])], dim=0)
+    #     labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
+    #     labels = labels.to(self.config['common']['device'])
+    #
+    #     features = F.normalize(features, dim=1)
+    #
+    #     similarity_matrix = torch.matmul(features, features.T)
+    #     # assert similarity_matrix.shape == (
+    #     #     self.args.n_views * self.args.batch_size, self.args.n_views * self.args.batch_size)
+    #     # assert similarity_matrix.shape == labels.shape
+    #
+    #     # discard the main diagonal from both: labels and similarities matrix
+    #     mask = torch.eye(labels.shape[0], dtype=torch.bool).to(self.config['common']['device'])
+    #     labels = labels[~mask].view(labels.shape[0], -1)
+    #     similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
+    #     # assert similarity_matrix.shape == labels.shape
+    #
+    #     # select and combine multiple positives
+    #     positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
+    #
+    #     # select only the negatives
+    #     negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
+    #
+    #     pdb.set_trace()
+    #
+    #     logits = torch.cat([positives, negatives], dim=1)
+    #     labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.config['common']['device'])
+    #
+    #     logits = logits / self.config['temperature']
+    #     return logits, labels
+
     def info_nce_loss(self, features):
-        labels = torch.cat([torch.arange(self.config['common']['batch_size']) for i in range(self.config['n_views'])], dim=0)
-        labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
-        labels = labels.to(self.config['common']['device'])
+        sample_num = self.config['common']['batch_size'] * self.config['n_views']
 
         features = F.normalize(features, dim=1)
-
         similarity_matrix = torch.matmul(features, features.T)
-        # assert similarity_matrix.shape == (
-        #     self.args.n_views * self.args.batch_size, self.args.n_views * self.args.batch_size)
-        # assert similarity_matrix.shape == labels.shape
 
-        # discard the main diagonal from both: labels and similarities matrix
-        mask = torch.eye(labels.shape[0], dtype=torch.bool).to(self.config['common']['device'])
-        labels = labels[~mask].view(labels.shape[0], -1)
-        similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
-        # assert similarity_matrix.shape == labels.shape
+        iden_matrix = np.identity(sample_num, dtype=np.bool8)
+        from_same_matrix = np.tile(np.identity(self.config['common']['batch_size']),
+                                   (self.config['n_views'], self.config['n_views']))
+        
+        positives_labels = torch.from_numpy(np.logical_xor(from_same_matrix, iden_matrix)).to(self.config['common']['device'])
+        negatives_labels = torch.from_numpy(np.logical_not(from_same_matrix)).to(self.config['common']['device'])
 
-        # select and combine multiple positives
-        positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
-
-        # select only the negatives
-        negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
+        positives = similarity_matrix[positives_labels].view(positives_labels.shape[0], -1)
+        negatives = similarity_matrix[negatives_labels].view(negatives_labels.shape[0], -1)
+        pdb.set_trace()
 
         logits = torch.cat([positives, negatives], dim=1)
-        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.config['common']['device'])
-
         logits = logits / self.config['temperature']
+        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.config['common']['device'])
         return logits, labels
 
     def train(self, train_loader):
